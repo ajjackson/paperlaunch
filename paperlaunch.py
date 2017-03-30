cebd063a57b444e89d2e4702d496029a1f84e2da
Adam J. Jackson <a.j.jackson@physics.org>
2017-03-30 15:10:22 +0100

    PEP8, fix CLI, fix compilation

--- paperlaunch.py
+++ paperlaunch.py
@@ -14,10 +14,10 @@ conf = ConfigParser()
 with open(join(conf_dir, 'settings.conf'), 'r') as f:
     conf.read_file(f)
 
-
 # Set up a new LaTeX paper project
 
-def paperlaunch(name='paperlaunch-project', github=False, private=True):
+
+def paperlaunch(name='paperlaunch-project', github=False, public=False):
     """
     Generate a LaTeX project for scientific publication
 
@@ -27,8 +27,8 @@ def paperlaunch(name='paperlaunch-project', github=False, private=True):
         private (bool): Make online repository private
     """
 
+    private = not public
 
-    
     if github:
         local_repo, remote_repo = create_repo_github(name, private=private)
 
@@ -36,12 +36,17 @@ def paperlaunch(name='paperlaunch-project', github=False, private=True):
         local_repo = create_repo_local(name)
 
     copy_tex_files(name)
+    git_index = local_repo.index
+    git_index.add(['paper.tex', 'si.tex', 'README.md', 'Makefile',
+                   'bibliography.bib'])
+    git_index.commit("Initial setup from templates")
+
 
 def copy_tex_files(name, **kwargs):
     from string import Template
     from shutil import copyfile
     from os import chdir
-    
+
     chdir(name)
 
     mapping = conf['template'].items()
@@ -60,8 +65,11 @@ def copy_tex_files(name, **kwargs):
     for target in ("Makefile", "bibliography.bib"):
         copyfile(join(template_dir, target), target)
 
+
 def create_repo_local(name):
-    git.Repo.init(path=join(curdir, name), mkdir=True)
+    local_repo = git.Repo.init(path=join(curdir, name), mkdir=True)
+    return local_repo
+
 
 def create_repo_github(name, private=True):
     """Create named repo on github, clone to local and return interfaces
@@ -79,14 +87,17 @@ def create_repo_github(name, private=True):
 
     gh = Github(login_or_token=token)
 
-    org = conf.get('github','org', fallback=None)
+    org = conf.get('github', 'org', fallback=None)
     if org is None:
         actor = gh.get_user()
     else:
         org = gh.get_organization('SMTG-UCL')
         actor = org
-    remote_repo = actor.create_repo(name, description="Description", 
-                                    gitignore_template="TeX", private=private)
+    remote_repo = actor.create_repo(
+        name,
+        description="Description",
+        gitignore_template="TeX",
+        private=private)
 
     # Clone to local for setup
     local_repo = git.Repo.clone_from(remote_repo.clone_url, join(curdir, name))
@@ -94,21 +105,28 @@ def create_repo_github(name, private=True):
     return (local_repo, remote_repo)
 
 
-
 def main():
     from argparse import ArgumentParser
-    parser=ArgumentParser(doc="Set up a new LaTeX project directory")
+    parser = ArgumentParser(description="Set up a new LaTeX project directory")
 
-    parser.add_argument('name', type=str,
-                        help="Project name. Used as folder name "
-                             "and repo address.")
-    parser.add_argument('--public', type='store_true',
-                        help="Don't make the repository private")
-    parser.add_argument('--github', '--gh', type='store_true',
-                        help="Make a repository on Github")
+    parser.add_argument(
+        'name',
+        type=str,
+        help="Project name. Used as folder name "
+        "and repo address.")
+    parser.add_argument(
+        '--public',
+        action='store_true',
+        help="Don't make the repository private")
+    parser.add_argument(
+        '--github',
+        '--gh',
+        action='store_true',
+        help="Make a repository on Github")
     args = parser.parse_args()
 
     paperlaunch(**vars(args))
+
 
 if __name__ == '__main__':
     main()
